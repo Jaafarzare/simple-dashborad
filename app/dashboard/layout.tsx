e467c14fcbb9fe11c75f65b9a25111ba97f52f46
Jaafarzare <Jaafar.zare@gmail.com>
2025-03-08 06:50:35 +0330

    Update dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import { metadata } from "./metadata";
-
+import { Toaster } from "@/components/ui/sonner";
 export { metadata };
 
 export default function DashboardLayout({
@@ -8,6 +8,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="container mx-auto py-4 px-4 min-h-screen">{children}</div>
+    <div className="container mx-auto py-4 px-4 min-h-screen">
+      {children}
+      <Toaster />
+    </div>
   );
 }
